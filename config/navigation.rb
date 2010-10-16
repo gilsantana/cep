d6cb711f37e16b651e7cc05610e78ab45fa09180
Sanderson Santana <sanderson.santana@gmail.com>
2010-10-16 17:28:31 -0300

    Commit

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,8 +1,12 @@
 SimpleNavigation::Configuration.run do |navigation|  
   navigation.items do |primary|
     primary.item :root, 'Home', root_path, :class=>"first" do |principal|
-      principal.item :controls, "Controles", controls_path do |controle|
-        controle.item :controls, "Novo Controle", new_control_path
+      principal.item :controls, "Controles", controls_path do |controles|
+        controles.item :controls, "Novo Controle", new_control_path
+        controles.item :controls, @control.nome, @control do |controle|
+          controle.item :controls, "Editar", edit_control_path(@control)
+        end
+        
       end
     end
   end
