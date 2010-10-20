0c3f34ae59ac20a701ac9e8ee9aa8a6eb9131e62
Sanderson Santana <sanderson.santana@gmail.com>
2010-10-20 01:43:49 -0300

    Commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     array = Array.new
     dados.order("tempo ASC").each_with_index do |i, index|
       if calculo.class.to_s=="Hash"
-        array << (i.send(calculo[:comparar].to_s, valor.to_s) < i.send(valor.to_s).round(2) ? marcador_vermelho(i.send(valor.to_s)) : i.send(valor.to_s))
+        array << (((i.send(calculo[:comparar_maior].to_s, valor.to_s) < i.send(valor.to_s).round(2)) or i.send(calculo[:comparar_menor].to_s, valor.to_s) > i.send(valor.to_s).round(2)) ? marcador_vermelho(i.send(valor.to_s)) : i.send(valor.to_s))
       else
         array << i.send(valor.to_s, calculo.to_s)
       end
