56874692d15d1c7881d83ebbcd0eb5fa5a393e73
Sanderson Santana <sanderson.santana@gmail.com>
2010-10-27 18:13:13 -0300

    Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ Qualidade::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_controller.relative_url_root = "/qualidade"
+  ENV['RAILS_RELATIVE_URL_ROOT'] = "/qualidade"
 end
